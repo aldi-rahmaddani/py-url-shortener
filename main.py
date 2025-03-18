20fee90be2b95f814cbcea32e1b242f2e1d6b6cc
Aldi Rahmaddani <aldi.rahmaddani12@gmail.com>
2025-03-18 14:25:24 +0700

    feat: add cors middleware

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import url_shortener
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Izinkan React lokal
+    allow_credentials=True,
+    allow_methods=["*"],  # Izinkan semua metode (GET, POST, DELETE, dll.)
+    allow_headers=["*"],  # Izinkan semua header
+)
+
 app.include_router(url_shortener.router)
